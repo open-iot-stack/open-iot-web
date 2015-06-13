70b39983bf9b464aec25511a7411d45120be9f90
Josh Peterson <jpetey88@gmail.com>
2015-06-13 15:03:52 -0400

    Fixed Promises

--- app/static/js/core.js
+++ app/static/js/core.js
@@ -3,47 +3,73 @@
   'use strict';
 
   var app = angular.module('BlackHoleApp', ['ngResource']);
+    app.factory('Token', function ($http,$q) {
+    return {
+    getToken: function () {
 
 
 
-
-    app.factory('Token', ['$http', function ($http) {
-  return {
-    getToken: function (callback) {
-      $http.get('/session').success( function (res) {
-       callback(res)
-      })
+      return $http.get('/session')
     }
   }
-}])
-
-
+})
 
       app. controller('WinkController', ['$scope', '$log', '$http', '$timeout','Token', function($scope, $log, $http,$timeout, Token) {
           $scope.getDevices = function () {
-            Token.getToken(function(res) { $scope.token = res });
-            var timeout = "";
-              var urlBase ="https://winkapi.quirky.com";
-              var poller = function () {
-                  $http({
-                      method: 'GET',
-                      url:urlBase+'/users/me/wink_devices',
-                      headers:{
-                        Authorization : 'Bearer ' + $scope.token
-                     }}).success(function (results) {
-                        //  $log.log(results);
-                          $scope.devices = results.data;
-                      }).
-                      error(function (error) {
-                          $log.log(error);
-                      });
-                   // continue to call the poller() function every 2 seconds
-        // until the timeout is cancelled
-                timeout = $timeout(poller, 2000);
-              }
-            poller();
-          }
+              var urlBase = "https://winkapi.quirky.com";
 
+              /*
+               var groups = function () {
+               $http({
+               method: 'GET',
+               url:urlBase+'/users/me/groups',
+               headers:{
+               Authorization : 'Bearer ' + $scope.token
+               }}).success(function (results) {
+               //  $log.log(results);
+               $scope.devices = results.data;
+               }).
+               error(function (error) {
+               $log.log(error);
+               });
+               // continue to call the poller() function every 2 seconds
+               // until the timeout is cancelled
+               timeout = $timeout(groups, 2000);
+               }
+               groups();
+               }
+               );
+               */
+
+
+              var poller = function () {
+
+                  Token.getToken().then(
+                      function (payload) {
+                          $scope.token = payload.data;
+                          var timeout = "";
+                          $http({
+                              method: 'GET',
+                              url: urlBase + '/users/me/wink_devices',
+                              headers: {
+                                  Authorization: 'Bearer ' + $scope.token
+                              }
+                          }).success(function (results) {
+                              //  $log.log(results);
+                              $scope.devices = results.data;
+                          }).
+                              error(function (error) {
+                                  $log.log(error);
+                              });
+                          // continue to call the poller() function every 2 seconds
+                          // until the timeout is cancelled
+                          timeout = $timeout(poller, 2000);
+                      });
+              }
+
+
+              poller();
+          }
             $scope.updateDeviceState = function (data) {
                  var urlBase ="https://winkapi.quirky.com";
                  //$scope.token = null
